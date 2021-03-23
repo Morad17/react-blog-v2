bf2f10ff24b2472e4834399904bdc9f69813945b
Morad17 <morad123@hotmail.co.uk>
2021-03-23 16:19:46 +0000

    creating navigation

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Link} from 'react-router-dom';
-
+import {Avatar} from 'antd'
 
 const navLinks = [
     {
@@ -22,20 +22,28 @@ const navLinks = [
 
 ]
 
-export default function Navigation () {
-    return(
-    <nav className="site-navigation">
-        <span>My react blog</span>
-        <ul>
-            { navLinks.map((link, index) => {
-                return(
-               <li key={index}>
-                   <Link to={link.path}>{link.title}</Link>
-                </li> )
-            })
-                
-            }
-        </ul>
+export default function Navigation ({user}) {
+    let [menuActive, setMenuActive] = useState(true)
 
+
+    return(
+    <nav className="site-navigation" role="navigation">
+        <span className="menu-title">My react blog</span>
+        <div className={`menu-content-container ${menuActive && "active"}`}>
+            <ul>
+                { navLinks.map((link, index) => {
+                    return(
+                <li key={index}>
+                    <Link to={link.path}>{link.title}</Link>
+                    </li> )
+                })
+                }
+            </ul>
+            <span className="menu-avatar-container">
+                <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" size={38} />
+                <span className="menu-avatar-name">{`${user.firstName} ${user.lastName}` }</span>
+            </span>
+        </div>
+        <i className="ionicons icon ion-ios-menu" onClick={(ev) => setMenuActive=(!menuActive)}/>
     </nav>)
 }
---
+++ src/pages/contact-me.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+
+export default function ContactMe (){
+    return <div>Contact Me</div>
+}
