1a69365655f20b80991dd7946c9a227b9f9ef379
Morad17 <morad123@hotmail.co.uk>
2021-03-24 19:53:37 +0000

    testing

---
+++ src/components/common/masonry-post.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function MasonryPost ({post, tagsOnTop}) {
+    return <div className="">test</div>
+}
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,14 @@
 import React from 'react'
-
+import {PostMasonry} from '../components/common'
+import trending from '../assets/mocks/trending'
 
 export default function Home (){
-    return <div>Home</div>
+    return (
+        <section className="container home">
+            <div className="row">
+                <h2>Trending</h2>
+                <PostMasonry posts={trending} columns={3}/>
+            </div>
+        </section>
+    )
 }
